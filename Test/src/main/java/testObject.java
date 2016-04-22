c46f98bcb43461407e4e2e1be445b3d99c6d3dea
Sunyi <sunyi03@meituan.com>
2016-04-22 19:58:24 +0800

    0422

---
+++ Test/src/main/java/testObject.java
@@ -0,0 +1,15 @@
+/**
+ * Created by sunyi on 16/4/19.
+ */
+public class testObject {
+    public static void main(String[] args) {
+        human aPerson =new human();
+        aPerson.breath();
+    }
+}
+
+ class human{
+    void breath(){
+        System.out.print("hu hu hu");
+    }
+}
